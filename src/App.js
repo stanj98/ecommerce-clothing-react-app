4b89387ed6b1c98fb8c83dc468bd787b0ed85259
Stanley John <infosoftallianze@gmail.com>
2021-03-17 15:48:04 +0530

    routing in progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 const HatsPage  = () => (
     <div>
@@ -8,12 +8,43 @@ const HatsPage  = () => (
     </div>
 )
 
+// const HomePage = props => {
+//     console.log(props);
+//     return (
+//       <div>
+//         <button onClick={() => props.history.push('/topics')}>Topics </button>
+//         <h1>HOME PAGE</h1>
+//       </div>
+//     );
+//   };
+  
+  const TopicsList = props => {
+    return (
+      <div>
+        <h1>TOPIC LIST PAGE</h1>
+        <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
+        <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
+        <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+      </div>
+    );
+  };
+  
+  const TopicDetail = props => {
+    return (
+      <div>
+        <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+      </div>
+    );
+  };
+
 function App() {
   return (
     <div>
         <Switch>
             <Route exact path = '/' component ={HomePage} />
             <Route path = '/hats' component ={HatsPage} />
+            <Route exact path='/blog/asdqw/topics' component={TopicsList} />
+            <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
         </Switch>
     </div>
   );
