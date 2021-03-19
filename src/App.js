df63f3fa6ff05a1c32afd26984f94e4189722193
Stanley John <infosoftallianze@gmail.com>
2021-03-19 08:43:26 +0530

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -18,25 +18,25 @@ const HatsPage  = () => (
 //     );
 //   };
   
-  const TopicsList = props => {
-    return (
-      <div>
-        <h1>TOPIC LIST PAGE</h1>
-        <button onClick={() => props.history.push('/topics') }></button>
-        <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-        <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-        <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
-      </div>
-    );
-  };
+//   const TopicsList = props => {
+//     return (
+//       <div>
+//         <h1>TOPIC LIST PAGE</h1>
+//         <button onClick={() => props.history.push('/topics') }></button>
+//         <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
+//         <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
+//         <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+//       </div>
+//     );
+//   };
   
-  const TopicDetail = props => {
-    return (
-      <div>
-        <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-      </div>
-    );
-  };
+//   const TopicDetail = props => {
+//     return (
+//       <div>
+//         <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+//       </div>
+//     );
+//   };
 
 function App() {
   return (
@@ -44,8 +44,8 @@ function App() {
         <Switch>
             <Route exact path = '/' component ={HomePage} />
             <Route path = '/hats' component ={HatsPage} />
-            <Route exact path='/blog/asdqw/topics' component={TopicsList} />
-            <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
+            {/* <Route exact path='/blog/asdqw/topics' component={TopicsList} />
+            <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} /> */}
         </Switch>
     </div>
   );
