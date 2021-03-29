9365c7bd36c2e9956e4d8db1f60d169144a65923
Stanley John <infosoftallianze@gmail.com>
2021-03-29 08:47:11 +0530

    created button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,8 @@ import './sign-in.styles.scss';
 
 import FormInput from '../form-input/form-input.component';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -34,7 +36,7 @@ class SignIn extends React.Component {
                     <FormInput name = 'email' type = 'email' value = {this.state.email} handleChange={this.handleChange} label = "email" required />
                     <FormInput name = 'password' type = 'password' value = {this.state.password} handleChange={this.handleChange} label = "password"  required />
 
-                    <input type = 'submit' value = 'Submit Form' />
+                    <CustomButton type = 'submit'> Sign In </CustomButton>
                 </form>
 
             </div>
