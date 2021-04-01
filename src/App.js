d1da4036a1f04cc5afa4592a261da3cd87c59cfd
Stanley John <infosoftallianze@gmail.com>
2021-04-01 09:05:10 +0530

    fbase config lesson

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route, Link } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import {auth} from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-        <Header />
-        <Switch>
-            <Route exact path = '/' component ={HomePage} />
-            <Route path = '/shop' component ={ShopPage} />
-            <Route path = '/signin' component ={SignInAndSignUpPage} />
-        </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          currentUser: null
+      }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+          this.setState({currentUser: user});
+
+          console.log(user)
+      })
+  }
+
+  componentWillMount() {
+      this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact path = '/' component ={HomePage} />
+                <Route path = '/shop' component ={ShopPage} />
+                <Route path = '/signin' component ={SignInAndSignUpPage} />
+            </Switch>
+        </div>
+    );
+  }
 }
 
 export default App;
