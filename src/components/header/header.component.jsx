1a97433f87cf1bc1b2f19d22a77187120c21f07a
Stanley John <infosoftallianze@gmail.com>
2021-04-03 08:54:27 +0530

    created sign in page with sign in component and sign in with Ggle

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,9 @@ import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = () => (
+import {auth} from '../../firebase/firebase.utils';
+
+const Header = ({currentUser}) => (
 
     <div className = "header">
         <Link className = 'logo-container' to = '/'>
@@ -17,6 +19,12 @@ const Header = () => (
             <Link className = 'option' to = '/shop'>
                 CONTACT
             </Link>
+            {
+                currentUser ? 
+                <div className ='option' onClick={() => auth.signOut()}> SIGN OUT </div>
+                :
+                <Link className = 'option' to = '/signin'>SIGN IN</Link>
+            }
         </div>
     </div>
 )
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -38,8 +38,10 @@ class SignIn extends React.Component {
                     <FormInput name = 'email' type = 'email' value = {this.state.email} handleChange={this.handleChange} label = "email" required />
                     <FormInput name = 'password' type = 'password' value = {this.state.password} handleChange={this.handleChange} label = "password"  required />
 
-                    <CustomButton type = 'submit'> Sign In </CustomButton>
-                    <CustomButton onClick={signInWithGoogle}> Sign In With Google</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type = 'submit'> Sign In </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In With Google</CustomButton>
+                    </div>
                 </form>
 
             </div>
